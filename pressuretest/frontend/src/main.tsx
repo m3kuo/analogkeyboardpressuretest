745c09cd078e36e292cb2885ea7795e2c1b6a289
YehAllen <allenyeh1105@gmail.com>
2026-04-10 12:14:37 -0600

    Remove pressuretest C# program

--- pressuretest/frontend/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './websocket';
-
-
-
-createRoot(document.getElementById("root")!).render(<App />);
